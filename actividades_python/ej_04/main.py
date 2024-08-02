a857d948b793ebdaa72a8809c951b9744c054d07
TatiFelizia <tatianafelizia@gmail.com>
2024-08-02 16:12:51 -0300

    feat: Finalización de actividad 04 de Python

--- actividades_python/ej_04/main.py
+++ actividades_python/ej_04/main.py
@@ -1,45 +1,50 @@
 from gpiozero import LED
 from gpiozero import PWMLED
-from time import sleep
+import time
 import math
 import ADS1x15
 
 #Declaro pines de leds
-led_rojo = PWMLED(19)
-led_azul = PWMLED(26)
+led_red = PWMLED(19)
+led_blue = PWMLED(26)
 
 #Declaro valores propios de los componentes
 vcc = 3.3
-v_term = 0, r_term = 0, r1 = 10000
-t = 0
+r1 = 10000
 x = 3977 # Factor de aumento del termistor
 
+#Configuro el adc
 ADS = ADS1x15.ADS1115(1, 0x48)
 ADS.setMode(ADS.MODE_SINGLE)
 ADS.setGain(ADS.PGA_4_096V)
 
+#Factor de conversión del adc
 f = ADS.toVoltage()
 
 while True :
-	# Obtengo valores de presión y temperatura
+	# Obtengo valores de presión y temperatura leídos por el adc
 	val_temperatura = ADS.readADC(1)
 	val_presion = ADS.readADC(3)
 
+	# Multiplico por el factor de conversión del adc obteniendo la tensión de cada uno
 	press = val_presion * f
 	temp = val_temperatura * f
 
 	# Cálculo de temperatura
 	v_term = (vcc * temp) / 4095
 	r_term = r1 / ((vcc / v_term) - 1)
-	t = (x / math.log10 ((r_term / r1) + (x / 298))) - 273.15
+	t = (x / math.log10((r_term / r1) + (x / 298.0))) - 273.15
 
+	#Condiciones relacionadas al encendido de los leds
+	#Si la presión es mayor a la temperatura, el led que prevalecerá encendido será el rojo
 	if (press > temp):
 		led_red.value = (press - temp) * 0.2
 
 		if (led_red.value > 1):
 			led_red.value = 1
 		time.sleep(1)
-		
+
+	#Si la presión es menor a la temperatura el led que prevalece es el azul
 	elif (press < temp):
 		led_blue.value = (temp - press) * 0.2
 		
@@ -53,6 +58,7 @@ while True :
 		time.sleep(1)
 		
 	# Imprimo los valores en consola
-	print ("Valor de temperatura: {0:.3f} V".format(temp))
-	print ("Valor de presión: {0:.3f} V".format(press))
+	print ("V de temperatura: {0:.3f} V\n".format(temp))
+	print ("Valor de temperatura: {0:.3f} T\n".format(t))
+	print ("V de presión: {0:.3f} V\n\n".format(press))
 	time.sleep(2)
